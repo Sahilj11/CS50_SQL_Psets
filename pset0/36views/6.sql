98668a1bb80e557c44ce8095affec67c72c4b304
Sahil jalandhara <sahiljalandhara123@gmail.com>
2023-10-29 12:08:42 +0530

    Completed 36 views pset

---
+++ pset0/36views/6.sql
@@ -0,0 +1,2 @@
+-- write a SQL query to find the average entropy of prints by Hiroshige, rounded to two decimal places. Call the resulting column “Hiroshige Average Entropy”.
+SELECT ROUND(AVG("entropy"),2) FROM "views" WHERE "artist" = 'Hiroshige';
